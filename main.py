1690ea6519b5145b1a64864ec341f5971031862b
Kalin <Kalin-chyikov@mail.ru>
2022-05-09 20:06:09 +0700

    3

--- main.py
+++ main.py
@@ -28,37 +28,47 @@ def register(update, context):
 def first_response(update, context):
     global sp
     imya_familiya = update.message.text
-    sp = imya_familiya.split()
-    for i in range(len(sp)):
-        sp[i] = sp[i].capitalize()
-    imya_familiya = ' '.join(sp)
-    update.message.reply_text(
-        f"{imya_familiya} в каком классе вы учитесь?\n"
-        'Введите параллель и букву своего класса через пробел')
-    return 2
+    sp = imya_familiya.split(' ')
+    if len(sp) < 2:
+        update.message.reply_text('Введи имя и фамилию через пробел')
+    else:
+        for i in range(len(sp)):
+            sp[i] = sp[i].capitalize()
+        imya_familiya = ' '.join(sp)
+        update.message.reply_text(
+            f"{imya_familiya} в каком классе вы учитесь?\n"
+            'Введите параллель и букву своего класса через пробел')
+        return 2
 
 
 def second_response(update, context):
     klass = update.message.text
     klass = klass.split()
-    for i in range(len(klass)):
-        if i == 0:
-            klass[i] = int(klass[i])
-        sp.append(klass[i])
-    print(sp)
-    logger.info(klass)
-    con = sqlite3.connect("cards_bd.sqlite")
-    cur = con.cursor()
-    zapros_bd = """INSERT INTO cods(imya,familiya,parallel,klass) """
-    zapros_bd += """VALUES("""
-    zapros_bd += """, """.join([f"'{key}'"
-                                for key in sp])
-    zapros_bd += """)"""
-    cur.execute(zapros_bd)
-    con.commit()
-    con.close()
-    update.message.reply_text("Карточка успешно зарегистрирована")
-    return ConversationHandler.END
+    if len(klass) < 2:
+        update.message.reply_text('Введи параллель числом и букву класса через пробел')
+    else:
+        for i in range(len(klass)):
+            if i == 0:
+                try:
+                    klass[i] = int(klass[i])
+                except ValueError:
+                    update.message.reply_text('Введи параллель числом и букву класса через пробел')
+                    return
+            sp.append(klass[i])
+        print(sp)
+        logger.info(klass)
+        con = sqlite3.connect("cards_bd.sqlite")
+        cur = con.cursor()
+        zapros_bd = """INSERT INTO cods(imya,familiya,parallel,klass) """
+        zapros_bd += """VALUES("""
+        zapros_bd += """, """.join([f"'{key}'"
+                                    for key in sp])
+        zapros_bd += """)"""
+        cur.execute(zapros_bd)
+        con.commit()
+        con.close()
+        update.message.reply_text("Карточка успешно зарегистрирована")
+        return ConversationHandler.END
 
 
 def stop(update, context):
